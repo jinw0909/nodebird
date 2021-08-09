2dc0ac12682e84f1711d68b17c1f3aa5da415691
Jinwoo Chang <>
2021-08-09 21:37:12 +0900

    changed port number to 80

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
+app.set('port', process.env.PORT || 80);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
