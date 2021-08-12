4bab85365f3bf4100eae76aa528818ac13873981
Jinwoo Chang <>
2021-08-12 16:09:11 +0900

    added redis memeory db to achieve lightsail stability because slowed down

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const helmet = require('helmet');
 const hpp = require('hpp');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
@@ -32,15 +38,21 @@ sequelize.sync({ force: false })
     });
 passportConfig();
 
-app.use(morgan('dev'));
-app.use(helmet({ contentSecurityPolicy: false}));
-app.use(hpp());
+if (process.env.NODE_ENV === 'production') {
+    app.enable('trust proxy');
+    app.use(morgan('combined'));
+    app.use(helmet({ contentSecurityPolicy: false }));
+    app.use(hpp());
+} else {
+    app.use(morgan('dev'));
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption = {
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -48,7 +60,13 @@ app.use(session({
         httpOnly: true,
         secure: false,
     },
-}));
+    store: new RedisStore({ client: redisClient }),
+    };
+    if (process.env.NODE_ENV === 'production') {
+    sessionOption.proxy = true;
+    // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
 
